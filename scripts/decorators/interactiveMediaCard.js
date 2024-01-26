9cec16ef848342a3b605acd843cd874728185bc0
kguie <kevin.guieba@gmail.com>
2024-01-26 16:27:53 -0400

    Initialisation du projet

--- scripts/decorators/interactiveMediaCard.js
+++
@@ -1,9 +0,0 @@
-//Add like event listener and image leventListener to media card
-function interactiveMediaCard(mediaCard) {
-  const $likeIconWrapper = mediaCard.querySelector(
-    ".media-card__text-container__likes"
-  );
-  handleLike($likeIconWrapper);
-
-  return mediaCard;
-}
